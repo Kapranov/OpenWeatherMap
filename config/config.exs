c5a730fa6aecc9ae60a1870418e27d620103b2fa
Oleg G.Kapranov <lugatex@yahoo.com>
2017-11-09 10:53:47 +0200

    added support redis

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,13 @@
 use Mix.Config
 import_config "#{Mix.env}.exs"
+
+config :redis_connection_pool, [
+  host: "127.0.0.1",
+  port: 6379,
+  password: "",
+  db: 0,
+  reconnect: :no_reconnect,
+  pool_name: :"Redis.Pool",
+  pool_size: 10,
+  pool_max_overflow: 1
+]
--- mix.exs
+++ mix.exs
@@ -13,14 +13,15 @@ defmodule OpenWeatherMap.Mixfile do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :redis_connection_pool]
     ]
   end
 
   defp deps do
     [
       {:httpoison, "~> 0.13.0"},
-      {:json, "~> 1.0.2"}
+      {:json, "~> 1.0.2"},
+      {:redis_connection_pool, "~> 0.1.5"}
     ]
   end
 end
